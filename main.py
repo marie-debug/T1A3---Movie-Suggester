5f5baa8cd18db8d9b15f7886d90c0b2c3a312566
Marion Akinyi <marion.akinyi@Marions-MacBook-Pro.local>
2022-09-20 17:25:03 +1000

    chore: add top movies per genre feature

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ load_dotenv()
 
 api_key = os.getenv('api_key')
 
-genres = requests.get('https://api.themoviedb.org/3/genre/movie/list?api_key='+api_key+'&language=en-US').json()
+genres_result = requests.get('https://api.themoviedb.org/3/genre/movie/list?api_key='+api_key+'&language=en-US').json()
+# print(genres_result['genres'])
+
+genresList = genres_result['genres']
+
 genre_dict={}
-for values in genres.values():
-    # print(values)
-    genresList = values
-    # print(genresList)
-    for genre in genresList:
-        # print(genre)
-        genre_dict[genre['name']]= genre['id']
+for genre in genresList:
+    # print(genre)
+    genre_dict[genre['name']]= genre['id']
+# print(genre_dict)
 
 
 print('Welcome to Movie suggestor, we help you find great shows!')
@@ -28,8 +29,14 @@ else:
     [print(key) for key,value in genre_dict.items()]
 
 
-# movies = requests.get('https://api.themoviedb.org/3/discover/movie?api_key='+api_key+'&language=en-US&with_genres=12&sort_by=vote_average.desc').json()
+movies = requests.get('https://api.themoviedb.org/3/discover/movie?api_key='+api_key+'&language=en-US&with_genres='+str(genre_dict[user_input])+'&sort_by=vote_average.desc').json()
+# print(movies)
 
-# top_movies= movies['results'][:11]
+top_movies= movies['results'][:11]
+# print(top_movies)
 
-
+for movie in top_movies:
+    print('\n================================')
+    print('Title: ' + movie['title'])
+    print('Summary: '+ movie['overview'])
+  
--- test.py
+++ test.py
@@ -1,8 +1,26 @@
-{'genres': 
-[
-    {'id': 28, 'name': 'Action'}, {'id': 12, 'name': 'Adventure'}, {'id': 16, 'name': 'Animation'}, {'id': 35, 'name': 'Comedy'}, {'id': 80, 'name': 'Crime'}, {'id': 99, 'name': 'Documentary'}, {'id': 18, 'name': 'Drama'}, {'id': 10751, 'name': 'Family'}, {'id': 14, 'name': 'Fantasy'}, {'id': 36, 'name': 'History'}, {'id': 27, 'name': 'Horror'}, {'id': 10402, 'name': 'Music'}, {'id': 9648, 'name': 'Mystery'}, {'id': 10749, 'name': 'Romance'}, {'id': 878, 'name': 'Science Fiction'}, {'id': 10770, 'name': 'TV Movie'}, {'id': 53, 'name': 'Thriller'}, {'id': 10752, 'name': 'War'}, {'id': 37, 'name': 'Western'}
-    ]
-    }
+{'genres':
+ [
+     {'id': 28, 'name': 'Action'},
+     {'id': 12, 'name': 'Adventure'},
+     {'id': 16, 'name': 'Animation'},
+     {'id': 35, 'name': 'Comedy'},
+     {'id': 80, 'name': 'Crime'},
+     {'id': 99, 'name': 'Documentary'},
+     {'id': 18, 'name': 'Drama'},
+     {'id': 10751, 'name': 'Family'},
+     {'id': 14, 'name': 'Fantasy'},
+     {'id': 36, 'name': 'History'},
+     {'id': 27, 'name': 'Horror'},
+     {'id': 10402, 'name': 'Music'},
+     {'id': 9648, 'name': 'Mystery'},
+     {'id': 10749, 'name': 'Romance'},
+     {'id': 878, 'name': 'Science Fiction'},
+     {'id': 10770, 'name': 'TV Movie'},
+     {'id': 53, 'name': 'Thriller'},
+     {'id': 10752, 'name': 'War'},
+     {'id': 37, 'name': 'Western'}
+ ]
+ }
 
 
 # for dic in v:
@@ -25,3 +43,43 @@
 {'id': 53, 'name': 'Thriller'}
 {'id': 10752, 'name': 'War'}
 {'id': 37, 'name': 'Western'}
+
+
+{
+    'page': 1,
+    'results': [
+        {
+            'adult': False,
+            'backdrop_path': None,
+            'genre_ids': [16, 12, 35, 10751],
+            'id': 1025488,
+            'original_language': 'en',
+            'original_title': 'SK80',
+            'overview': 'When desire is stronger than physical strength, and the consequences are immeasurable, we are left with a radical journey of an elderly person who wants to skate in order to feel alive again.',
+            'popularity': 1.055,
+            'poster_path': '/2RPcTudkd7CM2BCCQLYpUJpoVQc.jpg', 'release_date': '2022-07-06',
+            'title': 'SK80',
+            'video': False,
+            'vote_average': 10,
+            'vote_count': 1
+        },
+        {
+            'adult': False,
+            'backdrop_path': '/mdE69QWOkfHx3l2tG8gvC5jhhfN.jpg',
+            'genre_ids': [12],
+            'id': 1024637,
+            'original_language': 'en',
+            'original_title': 'Bugs',
+            'overview': 'After a virus decimates the wild rabbit populations, Bugs and three fellow lost souls roam the desert seeking answers from strangers as they journey towards the Light.',
+            'popularity': 0.6,
+            'poster_path': '/rg9W9WpUeyT6fDEpUYhTIwcpN8W.jpg',
+            'release_date': '2021-11-01',
+            'title': 'Bugs',
+            'video': False,
+            'vote_average': 10,
+            'vote_count': 1
+        }
+    ],
+    'total_pages': 920,
+    'total_results': 18398
+}
