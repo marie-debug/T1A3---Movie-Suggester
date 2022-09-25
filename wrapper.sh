ae8d8aab8fd11cf60c0bbbc869c47ea3c21a23ab
Marion Akinyi <marion.akinyi@Marions-MacBook-Pro.local>
2022-09-25 14:36:22 +1000

    refactor pip3 error message to display correctly

--- wrapper.sh
+++ wrapper.sh
@@ -9,8 +9,8 @@ fi
 if ! [[ -x "$(command -v pip3)" ]]
 then
   echo 'Error: 
-    This program runs on Python, but it looks like Python is not installed.
-    To install Python, check out https://installpython3.com/' >&2
+    it looks like pip3 is not installed.
+    To install pip3, check out https://pypi.org/project/pip/' >&2
   exit 1
 fi
 pip3 install -r requirements.txt 
